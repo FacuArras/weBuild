d4f5ed93291f46f87f75352f72bdb4d3d2319383
facundo <facundoarrascaetacba@gmail.com>
2022-05-23 09:52:01 -0300

    almost there

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var swiper = new Swiper(".reviews-slider", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
   loop: true,
   grabCursor: true,
   spaceBetween: 20,
@@ -11,6 +14,49 @@ var swiper = new Swiper(".reviews-slider", {
     },
     992: {
       slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  breakpoints: {
+    576: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".footer-slider", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    420: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    992: {
+      slidesPerView: 5,
     },
   },
 });
